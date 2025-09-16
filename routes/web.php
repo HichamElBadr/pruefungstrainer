b561b394af9b55ea6109213b371d04134bf01b55
HichamElBadr <hicham-elbadr@hotmail.de>
2025-09-16 14:44:45 +0200

    Create route for sql exercise

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SqlExerciseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+Route::prefix('it')->group(function () {
+    Route::get('sql-uebung', [SqlExerciseController::class, 'index'])
+        ->name('sql-uebung');
+});
+
+Route::prefix('wiso')->group(function () {});
+
+
+require __DIR__ . '/auth.php';
