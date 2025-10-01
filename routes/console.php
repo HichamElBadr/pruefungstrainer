fd6bdca6b96c537f62372fa06f44e53fc97beaf8
HichamElBadr <hicham-elbadr@hotmail.de>
2025-10-01 11:53:42 +0200

    Added a shedule which automaticly deletes old exercise rows

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+ function schedule(Schedule $schedule)
+{
+    $schedule->call(function () {
+        \App\Models\Exercise::where('created_at', '<', now()->subDay())->delete();
+    })->daily();
+}
